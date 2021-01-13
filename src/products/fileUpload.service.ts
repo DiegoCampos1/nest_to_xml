2c9dc8dfc3bb73fa731dca359eb1e0ae8189f4c3
Diego <diegodiko05@gmail.com>
2021-01-13 11:09:55 -0300

    Alterando resposta ao usuario

--- src/products/fileUpload.service.ts
+++ src/products/fileUpload.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 
+interface Response {
+  success: boolean;
+  message: string;
+}
 @Injectable()
 export class FileUploadService {
   private readonly logger: Logger = new Logger(FileUploadService.name);
 
-  async uploadXml(xml: any): Promise<string> {
+  async uploadXml(xml: any): Promise<Response> {
     const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 
     const s3 = new AWS.S3({
@@ -20,10 +24,13 @@ export class FileUploadService {
     });
     const key = 'martins_xml_default_prod.xml';
 
+    const folder = process.env.AWS_S3_FOLDER;
     const params: AWS.S3.PutObjectRequest = {
       Body: Buffer.from(xml, 'utf8'),
       Bucket: AWS_S3_BUCKET_NAME,
-      Key: `prod/${key}`,
+      // ideal ser dinamico o nome da pasta por ambiente
+
+      Key: `${folder}/${key}`,
       ContentEncoding: 'base64',
       ContentType: 'text/xml',
     };
@@ -31,10 +38,19 @@ export class FileUploadService {
     return new Promise((resolve, reject) => {
       s3.putObject(params, (err) => {
         if (err) {
+          const response = {
+            success: false,
+            message: `Erro ao salvar arquivo no s3`,
+          };
+
           this.logger.error(err);
-          return reject(err);
+          return reject(response);
         }
-        return resolve(`Arquivo salvo com sucesso: ${key}`);
+        const response = {
+          success: true,
+          message: `Arquivo salvo com sucesso: ${key}`,
+        };
+        return resolve(response);
       });
     });
   }
